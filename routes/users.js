338e4b8493345c625aeaf176b4e76baad0b232e8
Mohammed Abedin <mohammedabedin06@gmail.com>
2026-03-31 23:15:11 -0400

    param function

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,24 @@ router.get('/new', (req, res)=>{ // /users/new
 });
 
 
+const users = [
+    { firstName: "Mohammed", lastName: "Abedin", gender: "Male", age: 20},
+    {firstName: "Alex", lastName: "Hills", gender:"Male", age: 32}
+]
+
+// res.send(`Getting User Data: ${req.params.id}`);
+// });
+
+router.route('/:id').get((req, res) => {
+    res.send(`Getting User data for id: ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Deleting User data for id: ${req.params.id}`);
+}).put((req, res) => {
+    res.send(`Updating User data for id: ${req.params.id}`);
+});
+router.param("id", (req, res, next, id)=>{
+    console.log(id);
+    next();
+})
+
 module.exports = router;
